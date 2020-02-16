afd3943b8f53a2e8a7a181a540ad73b3860567d3
Luis Dasta <ldasta786@gmail.com>
2020-02-15 22:30:00 -0600

    initial commit

---
+++ scraper.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.amazon.com/FEICE-Stainless-Leathers-Waterproof-Business/dp/B074MWWTVL'
+desiredPrice = '150'
+headers = {
+'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
+}
+
+page = requests.get(url, headers=headers)
+
+soup = BeautifulSoup(page.content ,features='lxml')
+
+currentPrice = soup.select("#priceblock_saleprice")[0].get_text().strip()
+print(currentPrice)
